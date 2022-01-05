c6b543cf4a49cb06d839c3106efe45d4b233bd03
Fégan Pierre <fegan.pierre@gmail.com>
2022-01-05 12:40:25 +0100

    ch4.2 routeCollection

---
+++ src/infrastructure/routeCollection.ts
@@ -0,0 +1,67 @@
+import { Router } from "express
+
+type HttpVerb = "get" | "post"
+type ctor = new (...args: any[]) => any
+
+
+interface ControllerInformation {
+    controllerName: string
+    ctor: ctor
+    prefix: string
+}
+
+interface ActionInformation {
+    controllerName: string
+    methodName: string
+    httpVerb: HttpVerb
+    path: string
+}
+
+class RouteCollection {
+    private controllerInformations: ControllerInformation[]
+    private actionInformations: ActionInformation[]
+
+    constructor() {
+        this.controllerInformations = []
+        this.actionInformations = []
+    }
+
+    registerController(ctor: ctor, prefix: string) {
+        this.controllerInformations.push({
+            controllerName: ctor.name,
+            ctor,
+            prefix
+        })
+    }
+
+    registerAction(
+        controllerName: string,
+        methodName: string,
+        httpVerb: HttpVerb,
+        path?: string
+    ) {
+        this.actionInformations.push({
+            controllerName,
+            methodName,
+            httpVerb,
+            path: path || ""
+        })
+    }
+
+    setupRouter(router: Router) {
+        this.controllerInformations.forEach(c => {
+            const controller = new c.ctor()
+            const actions = this.actionInformations
+                .filter(a => a.controllerName === c.controllerName)
+
+            actions.forEach(a => {
+                const action = controller[a.methodName].bind(controller)
+                const route = `/${c.prefix}/${a.path}`
+                router[a.httpVerb](route, action)
+            })
+
+        })
+    }
+}
+
+export default new RouteCollection()
